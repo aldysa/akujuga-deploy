41c33972eae54472d72852ed3f027c2c0c000633
Aldysa <aldysayusuf@gmail.com>
2024-06-21 05:36:29 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,7 +1,10 @@
 const { Firestore } = require('@google-cloud/firestore');
  
 async function storeData(id, data) {
-  const db = new Firestore();
+  const db = new Firestore({
+    databaseId: process.env.DATABASE_ID,
+    
+  });
  
   const predictCollection = db.collection('prediction');
   return predictCollection.doc(id).set(data);
