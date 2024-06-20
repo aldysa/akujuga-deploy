f1ce2cb89263554c026cf573ba320f7f84b2ff8d
Aldysa <aldysayusuf@gmail.com>
2024-06-21 05:35:52 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -11,27 +11,30 @@ async function predictClassification(model_angka, model_huruf, image) {
  
         const classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
  
+        console.log('test');
         const predictionAngka = model_angka.predict(tensor);
         const scoreAngka = await predictionAngka.data();
         const confidenceScoreAngka = Math.max(...scoreAngka) * 100;
+        //console.log(scoreAngka);
 
         const predictionHuruf = model_huruf.predict(tensor);
         const scoreHuruf = await predictionHuruf.data();
         const confidenceScoreHuruf = Math.max(...scoreHuruf) * 100;
-        
+        //console.log(scoreHuruf);
         const mergedScore = [...scoreAngka, ...scoreHuruf];
         const idx = mergedScore.indexOf(Math.max(...mergedScore));
-        const temp_label = classes[idx];
-        confidenceScoreMerged = Math.max(...mergedScore) * 100;
+        const label = classes[idx];
+        const confidenceScore = Math.max(...mergedScore) * 100;
 
-       // const classResult = tf.argMax(prediction, 1).dataSync()[0];
-       // const label = classes[classResult];
+        // const classResult = tf.argMax(predictionAngka, 1).dataSync()[0];
+        // const label = classes[classResult];
  
+        console.log(label);
         let suggestion;
         
         suggestion = 'Silakan upload ulang untuk menerjemahkan bahasa isyarat lainnya';
 
-        return { confidenceScoreMerged, temp_label, suggestion };
+        return { confidenceScore, label, suggestion };
     } catch (error) {
         throw new InputError(`Terjadi kesalahan input: ${error.message}`)
     }
